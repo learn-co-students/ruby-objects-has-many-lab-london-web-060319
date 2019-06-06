db3b2fd1e3641f5417f9108540a9fec4c9d6233b
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-06 13:21:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  @@all=[]
+  
+  attr_accessor :artist, :name
+ 
+  def initialize(name)
+    @name = name
+    @@all<<self 
+  end 
+  
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+  
+  def self.all 
+    @@all 
+  end 
+ 
+end 
